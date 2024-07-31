5de21103792de13d7760d5339315004531a1160c
JhoanDiazArdila <jhoan.diaz.ardila@hotmail.com>
2024-07-31 11:39:42 -0500

    feat: :sparkles: Nueva funcion añadida
    La funcion de crear ciudades fue completada
    
    By:@Jhoan Diaz

--- funciones.py
+++ funciones.py
@@ -5,10 +5,68 @@ from leer_json import *
 
 
 
-Archivo="Gestion_tarjetas.json"
-Gestion= crear_leer_json(Archivo)
 
 
+desicion=("1.Si","2.No")
 
 
+def registrar_Ciudad():
+    clear_screen()
+    while True:
+        try:
+            Archivo="Registro_Ciudades.json"
+            Gestion= crear_leer_json(Archivo)
+            ciudad={}
+            postal=input("Escriba el codigo postal de la ciudad:\n-> ")
+            if Gestion.get(postal,None)==None:
+                nombre = input("Escriba el nombre de la ciudad:\n-> ").strip().lower()
+                poblacion=input("Escriba la poblacion estimada de la ciudad:\n-> ").strip()
+                pais=input("Escriba el pais al que pertenece la ciudad:\n-> ").strip().lower()
+                ciudad[postal]={
+                    "Nombre": nombre,
+                    "Poblacion": poblacion,
+                    "Pais": pais,
+                    "Codigo Postal": postal,
+                }
+                print("~"*100)
+                print("Estos son los datos a guardar")
+                print(ciudad)
+                print("~"*100)
+                print ("¿Desea continuar y guardar?")
+                for i in desicion:
+                    print(i)
+                opc1=int(input("--> "))
+                if opc1==2:
+                    print("Vuelva a empezar")  
+                    print("~"*100)  
+                    continue
+                elif opc1==1: 
+                    Gestion.update(ciudad)
+                    guardar_JSON(Gestion,Archivo)
+                else:
+                    print("Digite un valor correcto")
+                    continue 
+                print("¿Desea agregar otra ciudad?")
+                for i in desicion:
+                    print(i)
+                opc1=int(input("--> "))
+                if opc1==2:
+                    print("Saliendo...")  
+                    print("~"*100) 
+                    return
+                elif opc1==1:
+                    continue
+                else:
+                    print("Digite un valor correcto")
+                    print("~"*100) 
+                    continue
+            else:
+                print("El codigo postal ya existe")
+                print("~"*100) 
+                continue
+        except ValueError:
+            print("Digite un valor correcto")
+            print("~"*100) 
+            continue 
+        
 
